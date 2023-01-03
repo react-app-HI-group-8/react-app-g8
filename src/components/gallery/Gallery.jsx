dd6742df2fe5a59172c4cee647dff08c754f8049
Francisco Sabino <fdasabino@gmail.com>
2023-01-03 14:52:54 +0100

    added responsive gallery

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -5,10 +5,7 @@ import "./Gallery.scss";
 
 const Gallery = () => {
   return (
-    <section
-      className="gallery"
-      id="gallery"
-    >
+    <section className="gallery" id="gallery">
       <div className="carousel-wrapper">
         <Carousel
           axis={"horizontal"}
@@ -18,14 +15,8 @@ const Gallery = () => {
           showThumbs={false}
         >
           {images.map((img) => (
-            <div
-              className="carousel"
-              key={img.id}
-            >
-              <img
-                className="carousel_img"
-                src={img.link}
-              />
+            <div className="carousel" key={img.id}>
+              <img className="carousel_img" src={img.link} alt={img.title} />
               <div className="carousel_info">
                 <h3>{img.title}</h3>
                 <h4>{img.role}</h4>
