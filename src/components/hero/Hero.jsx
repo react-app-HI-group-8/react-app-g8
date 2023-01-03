3513dbec25be5881e6785af2e0ccfc7d0d088553
Francisco Sabino <fdasabino@gmail.com>
2023-01-03 10:14:34 +0100

    FEATURE: added hero img and hero circle bg
    Co-authored-by: MorganRapaport <MorganRapaport@users.noreply.github.com>
    Co-authored-by: Regan Ya <hens.virtuella.varld@gmail.com>

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,23 @@ import "./Hero.scss";
 const Hero = () => {
   return (
     <section className="hero" id="gallery">
-      Hero
+      <div className="left">
+        <h2>virtual world</h2>
+        <p>
+          WELCOME TO Monaki! MEET OUR COLLECTION OF 3.333 UNIQUE NFT. SUPPORT
+          AND JOIN OUR COMMUNITY, A NEW WORLD ARE WAITING FOR YOU.
+        </p>
+      </div>
+      <div className="right">
+        <div className="right_bg">
+          <img
+            className="hero_circle"
+            src="/imgs/hero_circle.png"
+            alt="hero_circle"
+          />
+          <img className="hero_img" src="/imgs/hero.png" alt="hero" />
+        </div>
+      </div>
     </section>
   );
 };
