71f16afb95dde6cc18dfc14820ad547a482881dc
Francisco Sabino <fdasabino@gmail.com>
2023-01-04 10:37:06 +0100

    FEATURE: Card component placeholder completed.
    Co-authored-by: Sammani Ariyarathne <SammaniAri@users.noreply.github.com>
    Co-authored-by: MorganRapaport <MorganRapaport@users.noreply.github.com>
    Co-authored-by: Regan Ya <hens.virtuella.varld@gmail.com>

--- src/components/marketplace/Marketplace.jsx
+++ src/components/marketplace/Marketplace.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import Card from "../card/Card";
 import "./Marketplace.scss";
 
 const Marketplace = () => {
   return (
     <section className="marketplace" id="marketplace">
-      Marketplace
+      <div className="left">
+        <Card />
+      </div>
+      <div className="right">
+        <h4>December 14, 2022</h4>
+        <h2>
+          RARE
+          <span>VIRTUAL WORLD</span>
+        </h2>
+        <p>Owned By OXOJ</p>
+        <p>
+          A NEW WAVE OF COLLECTIBLES IS ABOUT TO HIT THE ETHEREUM BLOCKCHAIN. A
+          SET OF 3.333 UNIQUE COLLECTABLES, THE ADDITION TO YOUR NFT COLLECTION
+          YOU’VE BEEN WAITING FOR.
+        </p>
+        <img src="/imgs/nft.png" alt="NFT" />
+      </div>
     </section>
   );
 };
