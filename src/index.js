82c5db903b197b0dae7ff7287bc353c3a5912570
Francisco Sabino <fdasabino@gmail.com>
2023-01-09 10:27:14 +0100

    FEATURE: Implement responsive navbar, added burger menu as well as added antd package.
    Co-authored-by: Sammani Ariyarathne <SammaniAri@users.noreply.github.com>
    Co-authored-by: MorganRapaport <MorganRapaport@users.noreply.github.com>
    Co-authored-by: Regan Ya <hens.virtuella.varld@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
